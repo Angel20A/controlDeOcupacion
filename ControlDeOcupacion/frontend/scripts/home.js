4a17dcc334e8545446c808803027abce0f027783
Angel <angelaragonf@gmail.com>
2025-03-18 17:42:52 -0600

    funcionamiento de configuracion con el dashboard

--- ControlDeOcupacion/frontend/scripts/home.js
+++ ControlDeOcupacion/frontend/scripts/home.js
@@ -150,65 +150,96 @@ async function progressBarData(porcentaje){
     }else{
         console.log("Porcentaje debe ser menor a 100");
     }
-    
-    /*const progress = document.querySelector(".progress-bar");
-    
-    console.log(progress);
-    
-    //rotaciones
-    progress.style.transform = `rotate(${grados}deg)`; //se rota el transform general
-    progress.style.setProperty("--rotateBefore" ,`rotate(-${grados}deg)`); //se rota el transform del before utilizando la variable --rotateBefore
-    
-    progress.style.setProperty("--rotateAfter" ,`rotate(-${grados}deg)`); ////se rota el transform del after utilizando la variable --after
-
-    progress.style.setProperty("--porcentaje", `"${porcentaje}%"`);
-    document.getElementById("progress-text").style.setProperty("--text", `-"${grados}deg"`);*/
 }
-
 //progressBarData(90);
 
 async function progressBar(contador, limInferior, limSuperior){
-    const cont = document.getElementById(contador).value;
-    const inferior = document.getElementById(limInferior).value;
-    const superior = document.getElementById(limSuperior).value;
+    const cont = parseInt(document.getElementById(contador).value);
+    const inferior = parseInt(document.getElementById(limInferior).value);
+    const superior = parseInt(document.getElementById(limSuperior).value);
+    console.log(cont);
+    console.log(inferior);
+    console.log(superior);
 
+    const h3Inferior = document.getElementById('limInferior');
+    const h3Superior = document.getElementById('limSuperior');
+    const h2Cantidad = document.getElementById('cantidad');
+    const span = document.getElementById('spanConfig');
+    span.innerHTML = "";
+    var porcentaje;
+
+    /*CASOS:
+    1. Si el valor de la cantidad está vacío y es menor a 0.    
+    2. Si el valor de la cantidad es menor al inferior.
+    3. Si el valor de la cantidad es mayor al superior.
+
+    4. Si el valor de el límite inferior está vacio o es menor a cero.
+    5. Si el límite inferior es mayor al límite superior.
     
-    //si el limite inferior o superior es menor a 0
-    if(inferior < 0 || superior <=0){
-        console.log("limite inferior y/o superior deben ser positivos. O superior no debe ser 0.");
+    6. Si el límite superior está vacio o es menor a cero.
+    */
+    if((inferior > 0 || inferior != NaN) && (superior > 0  || superior != NaN) && (superior > inferior)){
+        console.log("if 1");
+        console.log(superior + ">" + inferior);
+        h3Inferior.innerHTML = inferior;
+        h3Superior.innerHTML = superior;
+        porcentaje = Math.round((inferior * 100) / superior);
+        progressBarData(porcentaje);
+    
+        if(cont == NaN || cont == 0){
+            h2Cantidad.innerHTML = "0";
+        }
     }else{
-        //si el limite inferior es mayor al superior
-        console.log("inferior:" + inferior);
-        console.log("superior" + superior);
-        if(inferior > superior){
-            console.log("El limite inferior no puede ser mayor al superor.")
-        }else{
-            const h3Inferior = document.getElementById('limInferior');
-            const h3Superior = document.getElementById('limSuperior');
-            const h2Cantidad = document.getElementById('cantidad');
-            var porcentaje;
-            
+        span.innerHTML = "limite inferior y/o superior deben ser positivos. O superior no debe ser 0.";
+        console.log("limite inferior y/o superior deben ser positivos. O superior no debe ser 0.");
+    } 
 
-            if(cont == ""){
-                console.log("No hay valor en el contador");
-                h2Cantidad.innerHTML = "0";
-
-            }else{ //si el contador tiene un valor
-                h2Cantidad.innerHTML = cont;
-                porcentaje = Math.round((cont * 100) / superior);
-                progressBarData(porcentaje);
-            }
-
-            //si el limite inferior está vacio
-            if(inferior == ""){
+    if((cont > 0) && (cont !== NaN)){ //&& (superior > 0 || superior != "")
+        console.log("if 2");
+        if((cont > inferior) && (cont <= superior)){
+            if(inferior <= 0 || inferior == NaN){
                 h3Inferior.innerHTML = "0";
             }else{
                 h3Inferior.innerHTML = inferior;
-                porcentaje = Math.round((inferior * 100) / superior);
-                progressBarData(porcentaje);
             }
             h3Superior.innerHTML = superior;
-            
+            h2Cantidad.innerHTML = cont;
+            porcentaje = Math.round((cont * 100) / superior);
+            progressBarData(porcentaje);
+        }else{
+            span.innerHTML = "La cantidad debe estar entre el límite inferior y el límite superior.";
+            console.log("la cuenta manual no puede ser menor al límite inferior y mayor al límite superior.");
         }
+    }else{
+        h2Cantidad.innerHTML = "0";
+    }
+}
+
+async function resetearCuenta(checkbox){
+    const check = document.getElementById(checkbox).checked;
+    console.log(check);
+
+    const h3Inferior = document.getElementById('limInferior');
+    const h3Superior = document.getElementById('limSuperior');
+    const h2Cantidad = document.getElementById('cantidad');
+    const span = document.getElementById('spanConfig');
+    const cont = document.getElementById('cuentaManual');
+    const inferior = document.getElementById('limiteInferior');
+    const superior = document.getElementById('limiteSuperior');
+
+    if(check){
+        console.log('está seleccionado');
+        cont.value = "";
+        inferior.value = "";
+        superior.value = "";
+        
+        h2Cantidad.innerHTML = "";
+        h3Inferior.innerHTML = 0;
+        h3Superior.innerHTML = 0;
+        span.innerHTML = "";
+        progressBarData(0);
+
+    }else if(check == false){
+        console.log('no está seleccionado.');
     }
 }
