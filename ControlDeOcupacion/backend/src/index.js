6ca31a6f4621bf0c324d21692f4d2c1ba2ea0f32
Angel <angelaragonf@gmail.com>
2025-03-20 17:23:28 -0600

    Creacion de websocket en index.js

--- ControlDeOcupacion/backend/src/index.js
+++ ControlDeOcupacion/backend/src/index.js
@@ -2,19 +2,34 @@ import app from "./app.js"
 import { PORT } from "./config.js"
 import fs from "fs"
 import https from "https"
-import WebSocket from "ws"
+import { WebSocketServer } from "ws"
 
 /*https.createServer(app).listen(8080);
 https.createServer({
     cert: fs.readFileSync('./cert/server.cer'),
     key: fs.readFileSync('./cert/server.key'),
 }, app).listen(PORT);*/
-const server = https.createServer({
+
+
+
+//app.set("port", PORT);
+
+var server = https.createServer({
     cert: fs.readFileSync('./cert/server.cer'),
     key: fs.readFileSync('./cert/server.key'),
-}, app).listen(PORT);
+}, app);//.listen(PORT);
 
-const wss = new WebSocket.Server({server: server});
 
-wss.on("connection", function connection(conexion){
+//Creación del servidor de sockets e incorporación al servidor de la aplicación
+const wss = new WebSocketServer({server: server});
+
+wss.on("connection", (ws) => {
+    console.log("Nuevo cliente conectado!");
+
+    //enviar mensaje de bienvenida
+    ws.send("Hola este es un mensaje de bienvenida");
+});
+
+server.listen(PORT, ()=>{
+    console.log("Server corriendo en puerto: " + PORT);
 })
