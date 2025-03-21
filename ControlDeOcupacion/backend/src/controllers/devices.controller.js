4923f43159c165a38c6ffaaa17782d030912f2cf
Angel <angelaragonf@gmail.com>
2025-03-21 16:51:58 -0600

    Avances con websocket y llamada de devices

--- ControlDeOcupacion/backend/src/controllers/devices.controller.js
+++ ControlDeOcupacion/backend/src/controllers/devices.controller.js
@@ -1,13 +1,18 @@
+import { json } from "express";
 import { getConnection } from "../database/connection.js";
-//import sql from "mssql";
 
 export const getDevices = async (req, res) => {
     try{
         const pool = await getConnection();
         const result = await pool.request().query("select * from devices;");
-        console.log(result.recordset);
-        return res.json(result.recordset);
+        console.log(JSON.parse(result.recordsets));
+
+        //return res.json(result.recordset);
+        const devices = JSON.stringify(result.recordset);
+        return result.recordset;
     }catch(e){
-        return res.status(404).json({error: e.message});
+        //return res.status(404).json({error: e.message});
+        return {error: e.message};
     }
 }
+
--- ControlDeOcupacion/backend/src/index.js
+++ ControlDeOcupacion/backend/src/index.js
@@ -3,6 +3,7 @@ import { PORT } from "./config.js"
 import fs from "fs"
 import https from "https"
 import { WebSocketServer } from "ws"
+import { getDevices } from "./controllers/devices.controller.js"
 
 /*https.createServer(app).listen(8080);
 https.createServer({
@@ -12,22 +13,41 @@ https.createServer({
 
 
 
-//app.set("port", PORT);
-
 var server = https.createServer({
     cert: fs.readFileSync('./cert/server.cer'),
     key: fs.readFileSync('./cert/server.key'),
 }, app);//.listen(PORT);
-
+server;
 
 //Creación del servidor de sockets e incorporación al servidor de la aplicación
 const wss = new WebSocketServer({server: server});
 
 wss.on("connection", (ws) => {
     console.log("Nuevo cliente conectado!");
+    
+    //const devices = app.get('/devices', getDevices);
+    const devices = getDevices();
 
     //enviar mensaje de bienvenida
     ws.send("Hola este es un mensaje de bienvenida");
+    //ws.send(JSON.stringify(getDevices()));
+
+    //responder
+    ws.on("message", (message) =>{
+        console.log(message);
+
+        //validación de mensajes
+        if(message == "hola"){
+            ws.send("Hola!!");
+            ws.send(getDevices);
+            console.log(devices);
+        }else if(message == "adios"){
+            ws.send("Adios!!");
+        }else{
+            ws.send("otro mensaje")
+        }
+        
+    });
 });
 
 server.listen(PORT, ()=>{
